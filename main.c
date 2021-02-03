cb7d42689153deaffa434ae7b862dee86f5c8497
wat <wat@jirokichi.co.jp>
2021-02-03 18:04:20 +0900

    Create main.c
    最初のコミット

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by 白石 亘 on 2021/01/23.
+//  Copyright © 2021 白石 亘. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, My World!\n");
+    return 0;
+}
